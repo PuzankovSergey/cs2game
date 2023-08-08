96f32d5a082a5f0ddf5450fc8a6ff646bbf37368
PuzankovSergey <puzankov.sergey@gmail.com>
2023-08-08 17:34:10 +0600

    удаление звука

--- js/main.js
+++ js/main.js
@@ -173,26 +173,3 @@ let app = new Vue({
         }
     }
 })
-let audio = new Audio('audio/soundtrack.mp3')
-let audio_btn = document.querySelector('.btn__sound')
-let audoi_icon = document.querySelector('.btn__sound i')
-
-audio.muted = true;
-audio.autoplay = true;
-audio.volume = 0.2
-
-audio.addEventListener('loadmetadata', function() {
-    audio.currentTime= 0 + Math.random() * (audio.duration + 1 - 0)
-})
-
-audio_btn.addEventListener('click', function() {
-    if(audio.muted) {
-        audio.muted = false
-        audoi_icon.classList.add('fa-volume-up')
-        audoi_icon.classList.remove('fa-volume-off')
-    } else if(!audio.muted) {
-        audio.muted = true
-        audoi_icon.classList.remove('fa-volume-up')
-        audoi_icon.classList.add('fa-volume-off')
-    }
-})
